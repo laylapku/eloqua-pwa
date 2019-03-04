d3ecd4201cfb9296b27865e1749fdd8c8944fd0e
Layla <43167521+Leilei-Ouyang@users.noreply.github.com>
2019-03-05 01:47:51 +0800

    add a few basic features
    Former-commit-id: ad1515933ede709621666fa28f15b2fef2f2a57e

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
-import React, { Component } from 'react';
-import Navbar from './components/Navbar/Navbar.js';
-import Speaker from './components/Speaker/Speaker.js';
-import Speech from './components/Speech/Speech.js';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/Navbar/Navbar.js";
+import Speaker from "./components/Speaker/Speaker.js";
+import Speech from "./components/Speech/Speech.js";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      route: 'home',
+      route: "home",
       showSpeech: false,
-    }
+      active: false
+    };
   }
 
-  onRouteChange = (route) => {
-    if(route === 'home') {
-      this.setState({showSpeech: false});
-    } this.setState({route: route})
-  }
+  onRouteChange = route => {
+    if (route === "home") {
+      this.setState({ showSpeech: false });
+    }
+    this.setState({ route: route });
+  };
+
+  playPauseSpeech = () => {
+    const audio = document.getElementById("1");
+    if (this.state.active === false) {
+      this.setState({ active: true });
+      audio.play();
+    } else {
+      this.setState({ active: false });
+      audio.pause();
+    }
+  };
 
   render() {
     const { route } = this.state;
     return (
-      <div className='App'>
+      <div className="App">
         <Navbar onRouteChange={this.onRouteChange} />
-        { route === 'home'
-          ? <Speaker onRouteChange={this.onRouteChange} /> 
-          : <Speech />
-        }
+        {route === "home" ? (
+          <Speaker onRouteChange={this.onRouteChange} />
+        ) : (
+          <Speech playPauseSpeech={this.playPauseSpeech} />
+        )}
       </div>
     );
   }
